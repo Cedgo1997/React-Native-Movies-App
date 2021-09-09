931690d8430701ce6adacb7e2432b8137f6dbe4c
cedgo <cedgo1997@gmail.com>
2021-09-09 02:01:58 -0400

    Back button

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,16 +1,17 @@
 import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react';
-import { ActivityIndicator, Dimensions, Image, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, Dimensions, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
-import { RootStackParams } from '../navigation/Navigation';
+import { Navigation, RootStackParams } from '../navigation/Navigation';
 import { useMovieDetails } from '../hooks/useMovieDetails';
 import { MovieDetails } from '../components/MovieDetails';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const { height } = Dimensions.get('screen');
 
 interface Props extends StackScreenProps<RootStackParams, 'Details'> { }
 
-export const DetailsScreen = ({ route }: Props) => {
+export const DetailsScreen = ({ route, navigation }: Props) => {
 
   const movie = route.params;
   const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
@@ -32,6 +33,10 @@ export const DetailsScreen = ({ route }: Props) => {
           ? <ActivityIndicator size={35} color="grey" style={{ marginTop: 20 }} />
           : <MovieDetails movieFull={movieFull!} cast={cast!} />
       }
+      <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
+        <Icon name="arrow-back-outline" color="white" size={45} />
+      </TouchableOpacity>
+
     </ScrollView>
   );
 };
@@ -75,4 +80,10 @@ const styles = StyleSheet.create({
     borderBottomStartRadius: 25,
     overflow: 'hidden',
   },
+  backButton: {
+    position: 'absolute',
+    zIndex: 999,
+    elevation: 9,
+    margin: 10
+  }
 });
