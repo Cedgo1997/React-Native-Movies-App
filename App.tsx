93fd0d7b10b1c69422430b3540f1e83e92669f2b
cedgo <cedgo1997@gmail.com>
2021-09-09 17:01:10 -0400

    useFade playground, and hook ready

--- App.tsx
+++ App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import 'react-native-gesture-handler';
 import {Navigation} from './src/navigation/Navigation';
+import { FadeScreen } from './src/screens/FadeScreen';
 
 export const App = () => {
   return (
     <NavigationContainer>
-      <Navigation />
+      <FadeScreen/>
+      {/* <Navigation /> */}
     </NavigationContainer>
   );
 };
